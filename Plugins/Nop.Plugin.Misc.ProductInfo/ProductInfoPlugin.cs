366ff06adc6ee9ddfdcab3d9ea8f7adffa44a448
Linh Dinh <linhduydinh@gmail.com>
2020-08-18 23:43:16 +0700

    Add project files.

---
+++ Plugins/Nop.Plugin.Misc.ProductInfo/ProductInfoPlugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Misc.ProductInfo
+{
+    class ProductInfoPlugin: BasePlugin
+    {
+
+        public override void Install()
+        {
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            base.Uninstall();
+        }
+
+
+    }
+}
